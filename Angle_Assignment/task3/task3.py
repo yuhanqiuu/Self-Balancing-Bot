c00312ca254bbcdc73795664e0887c0fda83a770
Jen <yu204830@hotmail.com>
2025-01-26 11:57:43 -0800

    testing task4

---
+++ Angle_Assignment/task3/task3.py
@@ -0,0 +1,48 @@
+import time
+import serial 
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+
+def animate(i, dataList, ser):
+    # 'i' is a incrementing variable based upon frames = x argument
+    ser.write(b'g')                                     # Transmit the char 'g' to receive the Arduino data point
+    arduinoData_string = ser.readline().decode('utf-8').strip() # Decode receive Arduino data as a formatted string
+   # arduinoData_string = ser.readline().decode('ascii') # Decode receive Arduino data as a formatted string
+
+    try:
+        arduinoData_float = float(arduinoData_string)   # Convert to float
+        dataList.append(arduinoData_float)              # Add to the list holding the fixed number of points to animate
+
+    except:                                             # Pass if data point is bad                               
+        pass
+
+    dataList = dataList[-100:]                           # Fix the list size so that the animation plot 'window' is x number of points
+                                                        # keeping last 100 data points
+    
+    ax.clear()                                          # Clear last data frame
+    ax.plot(dataList)                                   # Plot new data frame
+    
+    ax.set_ylim([-180, 180])                              # Set Y axis limit of plot
+    ax.set_title("Arduino Gyroscope Angle Display")                        # Set title of figure
+    ax.set_ylabel("Angle measured with Gyroscope (degree)")                              # Set title of y axis 
+    ax.set_xlabel("Time")
+
+dataList = []                                           # Initialize list to hold data points for animation
+fig = plt.figure()                                      
+ax = fig.add_subplot(111)                               
+
+# Change the serial port and board rate to match the Arduino
+ser = serial.Serial("COM5", 9600)                       # Establish Serial object with COM port and BAUD rate to match Arduino Port/rate
+time.sleep(2)                                           # Time delay for Arduino Serial initialization 
+
+                                                        # Matplotlib Animation Fuction that takes takes care of real time plot.
+                                                        # Note that 'fargs' parameter is where we pass in our dataList and Serial object. 
+ani = animation.FuncAnimation(fig, animate, frames=100, fargs=(dataList, ser), interval=100) 
+
+plt.show()                                              
+ser.close()                                             
+
+
+
