6c20557aa58197bd238df1ccfb8a699b768c71e0
jxi01 <148392077+jxi01@users.noreply.github.com>
2025-02-12 19:13:40 -0800

    angle done

--- Motor/angle/angle.ino
+++ Motor/angle/angle.ino
@@ -1,7 +1,7 @@
-#define AIN1 2
-#define AIN2 3
-#define BIN2 4
-#define BIN1 5
+#define LEFT1 2 
+#define LEFT2 3
+#define RIGHT2 4
+#define RIGHT1 5
 
 #define MAX_ANGLE 90
 
@@ -34,11 +34,28 @@ void setup() {
   }
 }
 
+
+float rpm_to_pwm_left(float rpm) {
+  return 7.58 * exp(7.89E-3 * rpm);  // return pwm
+}
+
+float rpm_to_pwm_right(float rpm) {
+  return 8.33 * exp(7.55E-3 * rpm);  // return pwm
+}
+
 void forward(int pwm1, int pwm2){
-  analogWrite(AIN1, pwm1);   
-  digitalWrite(AIN2, LOW);   
-  analogWrite(BIN1, pwm2); 
-  digitalWrite(BIN2, LOW); 
+  analogWrite(LEFT1, pwm1);   
+  digitalWrite(LEFT2, LOW);   
+  analogWrite(RIGHT1, pwm2); 
+  digitalWrite(RIGHT2, LOW); 
+}
+
+// reverse fast decay
+void backward(int pwm1, int pwm2) {  
+  digitalWrite(LEFT1, LOW);   
+  analogWrite(LEFT2, pwm1);   
+  digitalWrite(RIGHT1, LOW); 
+  analogWrite(RIGHT2, pwm2);  
 }
 
 void loop() {
@@ -57,10 +74,19 @@ void loop() {
   }
 
   theta_n = k * (theta_n + theta_gn) + (1 - k) * theta_an;
+  Serial.println(theta_n);
 
   // Map angle (0° = 0% speed, MAX_ANGLE° = 100% speed)
-  int pwm = theta_n * 255 / 90;
-  if(theta_n > 30) forward(pwm, pwm);
+  float rpm_left = abs(theta_n) * 420 / 90;
+  float rpm_right = abs(theta_n) * 437 / 90;
+
+  if (theta_n > 0) {
+    forward(rpm_to_pwm_left(rpm_left), rpm_to_pwm_right(rpm_right));
+  } else if (theta_n < 0) {
+    backward(rpm_to_pwm_left(rpm_left), rpm_to_pwm_right(rpm_right));
+  } else {
+    forward(0, 0);
+  }
 
   //--------------------------------------
 }
