7ded3e1a49791f55c537c591223bb8a5f1459b1e
yuhanqiuu <qiuyuhan66@gmail.com>
2025-03-24 19:10:24 -0700

    update Camera Detection

--- Object_Detection/cam_setup.ino
+++ Object_Detection/cam_setup.ino
@@ -8,8 +8,8 @@ const char *WIFI_PASS = "hahahaha";
 WebServer server(80);
 
 static auto loRes = esp32cam::Resolution::find(320, 240);
-static auto midRes = esp32cam::Resolution::find(350, 530);
-static auto hiRes = esp32cam::Resolution::find(800, 600);
+// static auto midRes = esp32cam::Resolution::find(350, 530);
+// static auto hiRes = esp32cam::Resolution::find(800, 600);
 void serveJpg()
 {
     auto frame = esp32cam::capture();
@@ -37,23 +37,23 @@ void handleJpgLo()
     serveJpg();
 }
 
-void handleJpgHi()
-{
-    if (!esp32cam::Camera.changeResolution(hiRes))
-    {
-        Serial.println("SET-HI-RES FAIL");
-    }
-    serveJpg();
-}
+// void handleJpgHi()
+// {
+//     if (!esp32cam::Camera.changeResolution(hiRes))
+//     {
+//         Serial.println("SET-HI-RES FAIL");
+//     }
+//     serveJpg();
+// }
 
-void handleJpgMid()
-{
-    if (!esp32cam::Camera.changeResolution(midRes))
-    {
-        Serial.println("SET-MID-RES FAIL");
-    }
-    serveJpg();
-}
+// void handleJpgMid()
+// {
+//     if (!esp32cam::Camera.changeResolution(midRes))
+//     {
+//         Serial.println("SET-MID-RES FAIL");
+//     }
+//     serveJpg();
+// }
 
 void setup()
 {
@@ -63,7 +63,7 @@ void setup()
         using namespace esp32cam;
         Config cfg;
         cfg.setPins(pins::AiThinker);
-        cfg.setResolution(hiRes);
+        cfg.setResolution(loRes);
         cfg.setBufferCount(2);
         cfg.setJpeg(80);
 
@@ -81,12 +81,12 @@ void setup()
     Serial.print("http://");
     Serial.println(WiFi.localIP());
     Serial.println("  /cam-lo.jpg");
-    Serial.println("  /cam-hi.jpg");
-    Serial.println("  /cam-mid.jpg");
+    // Serial.println("  /cam-hi.jpg");
+    // Serial.println("  /cam-mid.jpg");
 
     server.on("/cam-lo.jpg", handleJpgLo);
-    server.on("/cam-hi.jpg", handleJpgHi);
-    server.on("/cam-mid.jpg", handleJpgMid);
+    // server.on("/cam-hi.jpg", handleJpgHi);
+    // server.on("/cam-mid.jpg", handleJpgMid);
 
     server.begin();
 }
